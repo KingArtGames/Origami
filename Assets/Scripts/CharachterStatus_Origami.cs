eb2c082f1e38af1c5611fb4e9c3b8efbcf989582
Thomas Peter Paul <thomas.peter.paul@kingart.local>
2016-04-16 20:17:26 +0200

    Items

---
+++ Assets/Scripts/CharachterStatus_Origami.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharachterStatus_Origami : MonoBehaviour {
+
+    public bool o_Fox = false;
+    public bool o_Fish = false;
+    public bool o_Fly = false;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+}
---
+++ Assets/Scripts/ItemStatus_Origami.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemStatus_Origami : MonoBehaviour {
+
+    public enum ItemEnum { Fox=0, Fish=1, Fly=2 };
+    public CharachterStatus_Origami charachterStatus_Origami;
+    public ItemEnum ItemPick;
+    public bool ItemGive;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        charachterStatus_Origami = other.GetComponent<CharachterStatus_Origami>();
+        if (ItemPick == ItemEnum.Fox)
+            charachterStatus_Origami.o_Fox=true;
+        if (ItemPick == ItemEnum.Fish)
+            charachterStatus_Origami.o_Fish = true;
+        if (ItemPick == ItemEnum.Fly)
+            charachterStatus_Origami.o_Fly = true;
+        Debug.Log(ItemPick);
+    }
+}

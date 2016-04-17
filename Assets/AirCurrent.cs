1fca51ff79ce78e1dd0bd72de4f354d9f6949585
Thomas Peter Paul <thomas.peter.paul@kingart.local>
2016-04-17 02:24:39 +0200

    FOX

---
+++ Assets/AirCurrent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirCurrent : MonoBehaviour {
+    private PlatformerCharacter2D_Origami platformerCharacter2D_Origami;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        platformerCharacter2D_Origami = other.GetComponent<PlatformerCharacter2D_Origami>();
+        Debug.Log(platformerCharacter2D_Origami);
+           // GetComponent<AreaEffector2D>().enabled= true;
+    }
+}

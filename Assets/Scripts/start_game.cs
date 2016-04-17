c75fc0484e4c358d55495150c4535d30077dc96f
Britta Struss <britta.struss@kingart.local>
2016-04-17 20:19:34 +0200

    button script

---
+++ Assets/Scripts/start_game.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class start_game : MonoBehaviour {
+
+	public void OnClick()  {
+
+		SceneManager.LoadScene("Level_Start");
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

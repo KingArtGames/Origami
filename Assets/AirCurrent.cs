1ca1e00d448007920ed235970bd392155c6f48d4
Thomas Peter Paul <thomas.peter.paul@kingart.local>
2016-04-18 00:49:03 +0200

    Char

--- Assets/AirCurrent.cs
+++ Assets/AirCurrent.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class AirCurrent : MonoBehaviour {
     private PlatformerCharacter2D_Origami platformerCharacter2D_Origami;
+    //private enum BirdStat;
 
     void OnTriggerStay2D(Collider2D other)
     {
         platformerCharacter2D_Origami = other.GetComponent<PlatformerCharacter2D_Origami>();
-        Debug.Log(platformerCharacter2D_Origami);
+        //BirdStat = platformerCharacter2D_Origami.CharacterShapes
            // GetComponent<AreaEffector2D>().enabled= true;
     }
 }
